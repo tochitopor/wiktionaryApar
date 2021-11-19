e68b3668e631e074d56248052d22ab33b11216cb
IAGanja <abichaika@yandex.ru>
2021-11-19 17:06:08 +0300

    дома посмотрю

--- src/main/java/WiktionaryApar.java
+++ src/main/java/WiktionaryApar.java
@@ -1,5 +1,23 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
 public class WiktionaryApar {
+    private static final String URL_FORMAT = "https://ru.wiktionary.org/wiki/%s";
+
     public static Entity getEntity(String s){
+        String url = String.format(URL_FORMAT,s);
+        try {
+            Document doc = Jsoup.connect(url).get();
+            System.out.println(doc.html());
+            /*Elements div = doc.getElementsByAttribute("morfotable ru");
+            doc.toString();*/
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
